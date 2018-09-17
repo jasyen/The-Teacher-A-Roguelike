c8b0cd6689b592f5df99351538ace5784e3b9e00
Jason Nguyen <justjasonnguyen@gmail.com>
2018-09-17 07:14:46 -0700

    Add enemies, collision for enemies, and turns

--- entity.py
+++ entity.py
@@ -2,11 +2,13 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-    def __init__(self, x, y, char, color):
+    def __init__(self, x, y, char, color, name, blocks=False):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
+        self.name = name
+        self.blocks = blocks
 
     def move(self, dx, dy):
         # Move the entity by given amount
@@ -15,3 +17,13 @@ class Entity:
 
     def get_x(self):
         return self.x
+
+
+# This function relates to entities but not to a specific entity so it's not in the class
+# Returns blocking entity at movement destination
+def get_blocking_entities_at_location(entities, destination_x, destination_y):
+    for entity in entities:
+        if entity.blocks and entity.x == destination_x and entity.y == destination_y:
+            return entity
+
+    return None
---
+++ game_states.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+# Represents whose turn it is
+class GameStates(Enum):
+    PLAYERS_TURN = 1
+    ENEMY_TURN = 2
+
